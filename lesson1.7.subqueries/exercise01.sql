33cd102c2a1029ba21a30d906079141ca1c73613
psounis <psounis@gmail.com>
2021-07-28 21:42:58 +0300

    Update exercise01.sql

--- lesson1.7.subqueries/exercise01.sql
+++ lesson1.7.subqueries/exercise01.sql
@@ -1,5 +1,5 @@
 SELECT c.customerName, MAX(p.amount) AS total_amount
-FROM customers c JOIN payments p
+FROM customers c LEFT JOIN payments p
 	ON c.customerNumber = p.customerNumber
 GROUP BY c.customerNumber
 ORDER BY 2 DESC;
